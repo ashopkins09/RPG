52b8904bde7b7a6b5324c931699db5a1fb659344
ashopkins09 <andrew.s.hopkins.09@gmail.com>
2025-05-08 19:17:55 -0500

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,1 +1,19 @@
+<?php
+$counterFile = "counter.txt";
+
+// If the file doesn't exist, create it
+if (!file_exists($counterFile)) {
+    file_put_contents($counterFile, "0");
+}
+
+// Read current count
+$count = (int)file_get_contents($counterFile);
+$count++;
+
+// Write updated count
+file_put_contents($counterFile, $count);
+
+// Return count
+echo $count;
+?>
 
